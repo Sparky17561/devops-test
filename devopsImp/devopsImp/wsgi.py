236f77bf1c4a6098897d2b36226441ad2a08c9be
Sparky17561 <studentjamdar@gmail.com>
2024-10-20 11:44:33 +0530

    docker-SE

---
+++ devopsImp/devopsImp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for devopsImp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devopsImp.settings')
+
+application = get_wsgi_application()
